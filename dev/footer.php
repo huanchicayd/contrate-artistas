b6c7237c0aa3798173d1c8131ac59befb464936d
Daniel Huanchicay <daniel@danielhuanchicay.com>
2017-10-26 18:31:09 -0200

    Estruturação do footer

--- dev/footer.php
+++ dev/footer.php
@@ -1,10 +1,16 @@
 <footer class="footer">
-  <div class="footer-newsletter">
+  <div class="footer-news">
     <div class="container">
       <div class="row">
-        <div class="col-lg-12 footer-newsletter__register">
-          <form class="footer-newsletter__form" action="index.html" method="post">
-            <input type="text" name="" value="" placeholder="seuemail@seuemail.com">
+        <div class="col-lg-3">
+          <img src="" alt="">
+        </div>
+        <div class="col-lg-9 footer-news__cadastro">
+          <p>Receba periodicamente as melhores dicas para eventos de alto impacto!</p>
+          <h4>Assine abaixo a nossa newsletter!</p>
+          <form class="footer-news__form" action="index.html" method="post">
+            <input type="email" name="" value="" placeholder="seuemail@seuemail.com" class="footer-news__form-email">
+            <button type="button" name="button" class="footer-news__form-btn">Cadastrar</button>
           </form>
         </div>
       </div>
@@ -31,27 +37,51 @@
         </p>
       </div>
     </div>
-    <div class="row footer-resume-links">
-      <div class="col-lg-4">
+    <div class="row footer-resume">
+      <div class="col-lg-4 footer-resume__atracao">
         <h4>Buscar atração artística</h4>
+        <div class="footer-resume__atracao-columns">
+          <ul>
+            <li><a href="#">Apresentador</a></li>
+            <li><a href="#">Apresentadora</a></li>
+            <li><a href="#">Ator</a></li>
+            <li><a href="#">Atriz</a></li>
+            <li><a href="#">Atleta</a></li>
+            <li><a href="#">Blogger/Youtuber</a></li>
+            <li><a href="#">Celebridade</a></li>
+          </ul>
+
+          <ul>
+            <li><a href="#">Comediante</a></li>
+            <li><a href="#">DJ</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-lg-2">
+        <h4>Institucional</h4>
         <ul>
-          <li><a href="#">Apresentador</a></li>
-          <li><a href="#">Apresentadora</a></li>
-          <li><a href="#">Ator</a></li>
-          <li><a href="#">Atriz</a></li>
-          <li><a href="#">Atleta</a></li>
-          <li><a href="#">Blogger/Youtuber</a></li>
-          <li><a href="#">Celebridade</a></li>
+          <li><a href="#">Quem somos</a></li>
         </ul>
       </div>
       <div class="col-lg-2">
-
+        <h4>Serviços</h4>
+        <ul>
+          <li><a href="#">Corporativo</a></li>
+          <li><a href="#">Particular</a></li>
+        </ul>
       </div>
       <div class="col-lg-2">
-
+        <h4>Portfólio de eventos</h4>
+        <ul>
+          <li><a href="#">Cases selecionados</a></li>
+          <li><a href="#">Galeria de fotos</a></li>
+        </ul>
       </div>
       <div class="col-lg-2">
-
+        <h4>Fale conosco</h4>
+        <ul>
+          <li><a href="#">Contato</a></li>
+        </ul>
       </div>
     </div>
   </div>
