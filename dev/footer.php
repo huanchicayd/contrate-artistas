2215c6a93803716fc3fa0934331d668383ab89b9
Daniel Huanchicay <daniel@danielhuanchicay.com>
2017-10-26 17:45:48 -0200

    Header geral concluído

--- dev/footer.php
+++ dev/footer.php
@@ -1,5 +1,72 @@
-<footer>
+<footer class="footer">
+  <div class="footer-newsletter">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12 footer-newsletter__register">
+          <form class="footer-newsletter__form" action="index.html" method="post">
+            <input type="text" name="" value="" placeholder="seuemail@seuemail.com">
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="container">
+    <div class="row footer-brand-wrap">
+      <div class="col-lg-6 footer-brand">
+        <a href="#">
+          <img src="images/logo.svg" alt="">
+        </a>
+      </div>
+      <div class="col-lg-6">
 
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-12 footer-about">
+        <p>
+          A Contrate Artistas é a escolha mais confiável para aqueles que visam qualidade, segurança e diversas opções na hora de contratar atrações artísticas para seus eventos especiais, sem pagar nada a mais por isso.
+        </p>
+        <p>
+          Com um atendimento 360° que alia anos de experiência e apurado conhecimento, consolidamos informações precisas que nos torna capaz de entender e mediar com equilíbrio, eficiência e excelência os ensejos de cada um dos contratantes, desde cerimônias particulares intimistas como casamentos e bodas às grandes experiências corporativas de alto impacto.
+        </p>
+      </div>
+    </div>
+    <div class="row footer-resume-links">
+      <div class="col-lg-4">
+        <h4>Buscar atração artística</h4>
+        <ul>
+          <li><a href="#">Apresentador</a></li>
+          <li><a href="#">Apresentadora</a></li>
+          <li><a href="#">Ator</a></li>
+          <li><a href="#">Atriz</a></li>
+          <li><a href="#">Atleta</a></li>
+          <li><a href="#">Blogger/Youtuber</a></li>
+          <li><a href="#">Celebridade</a></li>
+        </ul>
+      </div>
+      <div class="col-lg-2">
+
+      </div>
+      <div class="col-lg-2">
+
+      </div>
+      <div class="col-lg-2">
+
+      </div>
+    </div>
+  </div>
+  <div class="footer-copyright">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-9">
+          Contrate Artistas
+        </div>
+        <div class="col-lg-3">
+          Natio
+        </div>
+      </div>
+    </div>
+  </div>
 </footer>
 
 <script></script>
--- dev/index.php
+++ dev/index.php
@@ -1,12 +1,45 @@
 <?php include('header.php') ?>
 
-<main>
-  <div class="header-banner">
-    <div class="container">
-      
+<div class="header-banner">
+  <div class="container">
+    <h1 class="header-banner__chamada">Atendimento personalizado, agilidade, segurança e mais de<br> <strong class="header-banner__highlight-text">1.000 artistas disponíveis</strong> para o seu evento ser um sucesso!</h1>
+
+    <form class="header-banner__search-field">
+      <input type="search" name="" value="" placeholder="Pesquise atrações artísticas" class="header-banner__search-input">
+      <button type="button" name="button" class="header-banner__search-btn"></button>
+    </form>
+
+    <div class="header-banner__filtros">
+      <a href="#" class="header_banner__filtros-option">
+        <img src="images/filtro-show-icon.svg" alt="">
+        Show
+      </a>
+      <a href="#" class="header_banner__filtros-option">
+        <img src="images/filtro-ator-icon.svg" alt="">
+        Ator
+      </a>
+      <a href="#" class="header_banner__filtros-option">
+        <img src="images/filtro-atriz-icon.svg" alt="">
+        Atriz
+      </a>
+      <a href="#" class="header_banner__filtros-option">
+        <img src="images/filtro-celebridade-icon.svg" alt="">
+        Celebridade
+      </a>
+      <a href="#" class="header_banner__filtros-option">
+        <img src="images/filtro-comediante-icon.svg" alt="">
+        Comediante
+      </a>
+      <a href="#" class="header_banner__filtros-option">
+        <img src="images/filtro-vertodos-icon.svg" alt="">
+        Ver todos
+      </a>
     </div>
   </div>
+</div>
 
-</main>
+<div class="">
+
+</div>
 
 <?php include('footer.php') ?>
