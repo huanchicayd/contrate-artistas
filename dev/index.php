06867e61a56e639caa62c1067a20a9e780c36d42
Daniel Huanchicay <daniel@danielhuanchicay.com>
2017-10-31 15:33:42 -0200

    Estilização da sidebar, estilização do box de artista

--- dev/index.php
+++ dev/index.php
@@ -11,27 +11,27 @@
 
     <div class="header-banner__filtros">
       <a href="#" class="header_banner__filtros-option">
-        <img src="../images/filtro-show-icon.svg" alt="">
+        <img src="images/filtro-show-icon.svg" alt="">
         Show
       </a>
       <a href="#" class="header_banner__filtros-option">
-        <img src="../images/filtro-ator-icon.svg" alt="">
+        <img src="images/filtro-ator-icon.svg" alt="">
         Ator
       </a>
       <a href="#" class="header_banner__filtros-option">
-        <img src="../images/filtro-atriz-icon.svg" alt="">
+        <img src="images/filtro-atriz-icon.svg" alt="">
         Atriz
       </a>
       <a href="#" class="header_banner__filtros-option">
-        <img src="../images/filtro-celebridade-icon.svg" alt="">
+        <img src="images/filtro-celebridade-icon.svg" alt="">
         Celebridade
       </a>
       <a href="#" class="header_banner__filtros-option">
-        <img src="../images/filtro-comediante-icon.svg" alt="">
+        <img src="images/filtro-comediante-icon.svg" alt="">
         Comediante
       </a>
       <a href="#" class="header_banner__filtros-option">
-        <img src="../images/filtro-vertodos-icon.svg" alt="">
+        <img src="images/filtro-vertodos-icon.svg" alt="">
         Todas
       </a>
     </div>
@@ -43,46 +43,82 @@
       <div class="col-lg-3">
         <div class="sidebar">
           <div class="sidebar-stats">
-            302821 atrações artísticas
+            <span class="sidebar-stats__number">302821</span> atrações artísticas
           </div>
           <div class="sidebar-search">
             <h3>Palavra-chave</h3>
             <form class="" action="index.html" method="post">
-              <input type="search" name="" value="" placeholder="Campanha, Presença VIP....">
+              <input type="search" name="" value="" placeholder="Campanha, Presença VIP...." class="sidebar-search__input">
             </form>
           </div>
           <div class="sidebar-categorias">
             <h3>Todas as categorias</h3>
             <form class="" action="index.html" method="post">
-              <input type="checkbox" name="" value="false">
-              <label for="">Apresentador</label>
-              <input type="checkbox" name="" value="false">
-              <label for="">Apresentadora</label>
-              <input type="checkbox" name="" value="false">
-              <label for="">Apresentadora</label>
-              <input type="checkbox" name="" value="false">
-              Apresentadora
-              <input type="checkbox" name="" value="false">
-              Apresentadora
-              <input type="checkbox" name="" value="false">
-              Apresentadora
-              <input type="checkbox" name="" value="false">
-              Apresentadora
-              <input type="checkbox" name="" value="false">
-              Apresentadora
-              <input type="checkbox" name="" value="false">
-              Apresentadora
-              <input type="checkbox" name="" value="false">
-              Apresentadora
-              <input type="checkbox" name="" value="false">
-              Apresentadora
+              <div class="sidebar-categorias__checkbox">
+                <input type="checkbox" name="" value="false">
+                <label for="">Apresentador</label>
+              </div>
+              <div class="sidebar-categorias__checkbox">
+                <input type="checkbox" name="" value="false">
+                <label for="">Apresentadora</label>
+              </div>
+              <div class="sidebar-categorias__checkbox">
+                <input type="checkbox" name="" value="false">
+                <label for="">Ator</label>
+              </div>
+              <div class="sidebar-categorias__checkbox">
+                <input type="checkbox" name="" value="false">
+                <label for="">Atriz</label>
+              </div>
+              <div class="sidebar-categorias__checkbox">
+                <input type="checkbox" name="" value="false">
+                <label for="">Atleta</label>
+              </div>
+              <div class="sidebar-categorias__checkbox">
+                <input type="checkbox" name="" value="false">
+                <label for="">Celebridade</label>
+              </div>
+              <div class="sidebar-categorias__checkbox">
+                <input type="checkbox" name="" value="false">
+                <label for="">Comediante</label>
+              </div>
+              <div class="sidebar-categorias__checkbox">
+                <input type="checkbox" name="" value="false">
+                <label for="">DJ</label>
+              </div>
+              <div class="sidebar-categorias__checkbox">
+                <input type="checkbox" name="" value="false">
+                <label for="">Infantil</label>
+              </div>
+              <div class="sidebar-categorias__checkbox">
+                <input type="checkbox" name="" value="false">
+                <label for="">Jornalista</label>
+              </div>
+              <div class="sidebar-categorias__checkbox">
+                <input type="checkbox" name="" value="false">
+                <label for="">Palestrante</label>
+              </div>
+              <div class="sidebar-categorias__checkbox">
+                <input type="checkbox" name="" value="false">
+                <label for="">Show</label>
+              </div>
+              <div class="sidebar-categorias__checkbox">
+                <input type="checkbox" name="" value="false">
+                <label for="">Teatro</label>
+              </div>
+              <div class="sidebar-categorias__checkbox">
+                <input type="checkbox" name="" value="false">
+                <label for="">Youtuber</label>
+              </div>
             </form>
           </div>
         </div>
       </div>
       <div class="col-lg-9">
         <div class="home-content">
-          <div class="home-content__status"></div>
+          <div class="home-content__status">
+            Filtro01 | Filtro02
+          </div>
           <div class="home-content__artista-wrap">
             <div class="artista-box">
               <div class="artista-box__profile">
@@ -96,25 +132,63 @@
               </div>
               <div class="artista-box__social-media">
                 <a href="#">
-                  <div class="">
+                  <div class="artista-box__social-media__icon-wrap">
 
                   </div>
                   5.5kk
                 </a>
                 <a href="#">
-                  <div class="">
+                  <div class="artista-box__social-media__icon-wrap">
 
                   </div>
                   5.5kk
                 </a>
                 <a href="#">
-                  <div class="">
+                  <div class="artista-box__social-media__icon-wrap">
 
                   </div>
                   5.5kk
                 </a>
                 <a href="#">
-                  <div class="">
+                  <div class="artista-box__social-media__icon-wrap">
+
+                  </div>
+                  5.5kk
+                </a>
+              </div>
+              <button type="button" name="button" class="artista-box__button">Orçamento</button>
+            </div>
+            <div class="artista-box">
+              <div class="artista-box__profile">
+                <img src="images/artista-img.jpg" alt="Nome do artista">
+                <div class="artista-box__profile-info">
+                  <h2 class="artista-box__profile-title">George Henrique e Rodrigo</h2>
+                  <div class="artista-box__profile-tag">
+                    <span>Show</span>
+                  </div>
+                </div>
+              </div>
+              <div class="artista-box__social-media">
+                <a href="#">
+                  <div class="artista-box__social-media__icon-wrap">
+
+                  </div>
+                  5.5kk
+                </a>
+                <a href="#">
+                  <div class="artista-box__social-media__icon-wrap">
+
+                  </div>
+                  5.5kk
+                </a>
+                <a href="#">
+                  <div class="artista-box__social-media__icon-wrap">
+
+                  </div>
+                  5.5kk
+                </a>
+                <a href="#">
+                  <div class="artista-box__social-media__icon-wrap">
 
                   </div>
                   5.5kk
