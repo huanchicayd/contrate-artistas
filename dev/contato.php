7e2cdbb5148df4d91a3fc62046d30399400ed452
Daniel Huanchicay <huanchicayd@gmail.com>
2018-01-08 11:29:19 -0200

    Estruturação da página de contato

--- dev/contato.php
+++ dev/contato.php
@@ -1,7 +1,126 @@
 <?php include('header.php') ?>
 
-<main style='margin: 150px 0; text-align: center'>
-    <h1>Contato</h1>
+<main class="contato">
+    <section class="contato__banner">
+        
+    </section>
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="breadcrumb">
+                        Você está em: Home >
+                        <span class="breadcrumb__actual-page breadcrumb__active02">Contato</span>
+                    </div>
+                    <div class="section-title section-title--left">
+                        <h2 class="section-title__title">Solta a voz!</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xl-8">
+                    <div>
+                        <h4>Sinta-se livre para conectar-se conosco!</h4>
+                        <p>
+                             Estaremos prontos para atendê-lo e ajudá-lo a encontrar a atração perfeita para o seu evento.
+                        </p>
+                    </div>
+                    <div>
+                        <form action="">
+                            <label for="name">
+                                Nome
+                            </label>
+                            <input type="text" id="name">
+                            <label for="email">
+                                Email
+                            </label>
+                            <input type="text" id="email">
+                            <label for="phone">
+                                Telefone
+                            </label>
+                            <input type="text" id="phone">
+                            <label for="desc_evento">
+                                Descrição do evento
+                            </label>
+                            <textarea name="" id="desc_evento" cols="30" rows="10"></textarea>
+                            <button type="submit" class="btn-primary">Enviar</button>
+                        </form>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-push-1">
+                    <div>
+                        Social Icons
+                    </div>
+                    <div>
+                        Address
+                    </div>
+                    <div>
+                        Phone
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xl-12">
+                    Mail
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="contato__nossos-clientes">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="section-title">
+                        <h2 class="section-title__title">Nossos clientes</h2>
+                    </div>
+                    <div class="nossos-clientes__slider">
+                        <a href="#" class="nossos-clientes__item">
+                            <img src="images/coca-logo.png" alt="">
+                        </a>
+                        <a href="#" class="nossos-clientes__item">
+                            <img src="images/coca-logo.png" alt="">
+                        </a>
+                        <a href="#" class="nossos-clientes__item">
+                            <img src="images/coca-logo.png" alt="">
+                        </a>
+                        <a href="#" class="nossos-clientes__item">
+                            <img src="images/coca-logo.png" alt="">
+                        </a>
+                        <a href="#" class="nossos-clientes__item">
+                            <img src="images/coca-logo.png" alt="">
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="nossos-servicos">
+        <div class="section-title">
+            <h2 class="section-title__title">Nossos serviços</h2>
+        </div>
+        <div class="nossos-servicos__wrap">
+            <div class="nossos-servicos__corporativo">
+                <div class="nossos-servicos__content-left">
+                    <h3>Corporativo</h3>
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis delectus dolore necessitatibus ipsam quidem voluptatibus
+                        ut quod, eaque maiores facere saepe deleniti quasi consectetur soluta, at est aliquam, excepturi vitae!
+                    </p>
+                    <button class="btn-primary nossos-servicos__btn">Saiba mais</button>
+                </div>
+            </div>
+            <div class="nossos-servicos__particular">
+                <div class="nossos-servicos__content-right">
+                    <h3>Particular</h3>
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam voluptates velit mollitia, inventore est officiis explicabo
+                        fugiat, ipsa tempora ut, quo odio. Praesentium dolor illo ullam perferendis, modi magnam pariatur!
+                    </p>
+                    <button class="btn-primary nossos-servicos__btn">Saiba mais</button>
+                </div>
+            </div>
+        </div>
+    </section>
 </main>
 
 <?php include('footer.php') ?>
